423868592fb7a8d1ee297da012ef52c298d4a6aa
aroliwa <aroliwa@gmail.com>
2017-11-30 11:12:34 +0100

    Update Week1.R

--- Week1.R
+++ Week1.R
@@ -1,8 +1,8 @@
-file.create("plik.csv")
+file.create("file.csv")
 download.file(url = "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv", 
-              destfile = "plik.csv", 
+              destfile = "file.csv", 
               method = "auto")
-data <- read.csv(file = "plik.csv", header = TRUE, sep = ",")
+data <- read.csv(file = "file.csv", header = TRUE, sep = ",")
 
 ########################################
 
@@ -15,11 +15,11 @@ summary(as.factor(data$VAL)) #24 - 53
 ########################################
 
 library(openxlsx)
-file.create("plikXLS.xlsx")
+file.create("fileXLSX.xlsx")
 download.file(url = "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx", 
-              destfile = "plikXLS.xlsx", 
+              destfile = "fileXLSX.xlsx", 
               method = "auto")
-dat <- read.xlsx(xlsxFile = "plikXLS.xlsx", sheet = 1, rows = 18:23, cols = 7:15)
+dat <- read.xlsx(xlsxFile = "fileXLSX.xlsx", sheet = 1, rows = 18:23, cols = 7:15)
 
 sum(dat$Zip*dat$Ext,na.rm=T) #36534720
 
@@ -37,10 +37,10 @@ xmlDataFrame %>%
 ########################################
 
 library(data.table)
-file.create("plik2.csv")
+file.create("file2.csv")
 download.file(url = "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv", 
-              destfile = "plik2.csv", method = "auto")
-DT <- fread("plik2.csv", header = TRUE)
+              destfile = "file2.csv", method = "auto")
+DT <- fread("file2.csv", header = TRUE)
 
 start <- Sys.time()
 DT[,mean(pwgtp15),by=SEX]
